109b46c9512d06bae739ec8d4fe14d44a06f2e41
nakao-kodai <152832000+hc-nakao-kodai@users.noreply.github.com>
2024-12-11 11:12:54 +0900

    Create fizzbuzz.ts

---
+++ fizzbuzz.ts
@@ -0,0 +1,17 @@
+// FizzBuzzのコード
+function fizzBuzz(n: number): void {
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+// 1から100までのFizzBuzzを実行
+fizzBuzz(100);
